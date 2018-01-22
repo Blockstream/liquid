153909a99d224bb9f5ddf221c35f44296c1653e5
Gregory Sanders <gsanders87@gmail.com>
2018-01-22 13:13:41 -0500

    change prefixes from previous Liquid Beta network

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -419,9 +419,9 @@ public:
             0,
             0
         };
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,58);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,40);
-        base58Prefixes[BLINDED_ADDRESS]= std::vector<unsigned char>(1,13);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,57);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,39);
+        base58Prefixes[BLINDED_ADDRESS]= std::vector<unsigned char>(1,12);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
