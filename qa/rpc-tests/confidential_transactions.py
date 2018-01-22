c5265836c3fc76c368ea8b76dbc38d62672537a6
Gregory Sanders <gsanders87@gmail.com>
2018-01-22 10:57:21 -0500

    Revert "remove issuances"
    This reverts commit 8a8f5f5a7fe1ff92b57dd74c272e9cfc91890af4.

--- qa/rpc-tests/confidential_transactions.py
+++ qa/rpc-tests/confidential_transactions.py
@@ -226,7 +226,259 @@ class CTTest (BitcoinTestFramework):
         assert_equal(self.nodes[0].listissuances()[0]["assetlabel"], "bitcoin")
         assert_equal(len(self.nodes[0].listissuances()), 1)
 
-        # Bitcoin is the *only* issuance for Liquid v1, removed other tests
+        # Unblinded issuance of asset
+        issued = self.nodes[0].issueasset(1, 1, False)
+        assert_equal(self.nodes[0].getwalletinfo()["balance"][issued["asset"]], 1)
+        assert_equal(self.nodes[0].getwalletinfo()["balance"][issued["token"]], 1)
+        # Quick unblinded reissuance check, making 2*COIN total
+        self.nodes[0].reissueasset(issued["asset"], 1)
+
+        testAssetHex = issued["asset"]
+        self.nodes[0].generate(1)
+        self.sync_all()
+
+        issued2 = self.nodes[0].issueasset(2, 1)
+        test_asset = issued2["asset"]
+        assert_equal(self.nodes[0].getwalletinfo(test_asset)['balance'], Decimal(2))
+
+        assert_equal(self.nodes[1].getwalletinfo(test_asset)['balance'], Decimal(0))
+
+        # Assets balance checking, note that accounts are completely ignored because
+        # balance queries with accounts are horrifically broken upstream
+        assert_equal(self.nodes[0].getbalance("*", 0, False, "bitcoin"), self.nodes[0].getbalance("accountsareignored", 0, False, "bitcoin"))
+        assert_equal(self.nodes[0].getwalletinfo()['balance']['bitcoin'], self.nodes[0].getbalance("accountsareignored", 0, False, "bitcoin"))
+
+        # Send some bitcoin and other assets over as well to fund wallet
+        addr = self.nodes[2].getnewaddress()
+        self.nodes[0].sendtoaddress(addr, 5)
+        self.nodes[0].sendmany("", {addr:1, self.nodes[2].getnewaddress():13}, 0, "", [], {addr:test_asset})
+
+        self.sync_all()
+
+        # Should have exactly 1 in change(trusted, though not confirmed) after sending one off
+        assert_equal(self.nodes[0].getbalance("doesntmatter", 0, False, test_asset), 1)
+        assert_equal(self.nodes[2].getunconfirmedbalance(test_asset), Decimal(1))
+
+        b_utxos = self.nodes[2].listunspent(0, 0, [], True, "bitcoin")
+        t_utxos = self.nodes[2].listunspent(0, 0, [], True, test_asset)
+
+        assert_equal(len(self.nodes[2].listunspent(0, 0, [])), len(b_utxos)+len(t_utxos))
+
+        # Now craft a blinded transaction via raw api
+        rawaddrs = []
+        for i in range(2):
+            rawaddrs.append(self.nodes[1].getnewaddress())
+        raw_assets = self.nodes[2].createrawtransaction([{"txid":b_utxos[0]['txid'], "vout":b_utxos[0]['vout'], "nValue":b_utxos[0]['amount']}, {"txid":b_utxos[1]['txid'], "vout":b_utxos[1]['vout'], "nValue":b_utxos[1]['amount'], "asset":b_utxos[1]['asset']}, {"txid":t_utxos[0]['txid'], "vout":t_utxos[0]['vout'], "nValue":t_utxos[0]['amount'], "asset":t_utxos[0]['asset']}], {rawaddrs[1]:Decimal(t_utxos[0]['amount']), rawaddrs[0]:Decimal(b_utxos[0]['amount']+b_utxos[1]['amount']-Decimal("0.01")), "fee":Decimal("0.01")}, 0, {rawaddrs[0]:b_utxos[0]['asset'], rawaddrs[1]:t_utxos[0]['asset'], "fee":b_utxos[0]['asset']})
+
+        # Sign unblinded, then blinded
+        signed_assets = self.nodes[2].signrawtransaction(raw_assets)
+        blind_assets = self.nodes[2].blindrawtransaction(raw_assets)
+        signed_assets = self.nodes[2].signrawtransaction(blind_assets)
+
+        # And finally send
+        self.nodes[2].sendrawtransaction(signed_assets['hex'])
+        self.nodes[2].generate(101)
+        self.sync_all()
+
+        # Destroy assets
+        pre_destroy_btc_balance = self.nodes[2].getwalletinfo()['balance']['bitcoin']
+        self.nodes[2].destroyamount('bitcoin', 2) # Destroy 2 BTC
+        self.nodes[2].generate(1)
+        self.sync_all()
+
+        issuedamount = self.nodes[0].getwalletinfo()['balance'][issued["token"]]
+        assert_equal(issuedamount, Decimal('1.0'))
+        self.nodes[0].destroyamount(issued["token"], issuedamount) # Destroy all reissuance tokens of one type
+
+        self.nodes[0].generate(1)
+        self.sync_all()
+        assert(issued["token"] not in self.nodes[0].getinfo()['balance'])
+
+        # Test various issuance and auditing paths
+
+        issuancedata = self.nodes[0].issueasset(Decimal('0.00000002'), Decimal('0.00000001')) #2 of asset, 1 reissuance token
+        self.nodes[1].generate(1)
+        self.sync_all()
+        assert_equal(self.nodes[0].getwalletinfo()["balance"][issuancedata["asset"]], Decimal('0.00000002'))
+        assert_equal(self.nodes[0].getwalletinfo()["balance"][issuancedata["token"]], Decimal('0.00000001'))
+        self.nodes[0].reissueasset(issuancedata["asset"], Decimal('0.00000001'))
+        self.sync_all()
+        assert_equal(self.nodes[0].getwalletinfo()["balance"][issuancedata["asset"]], Decimal('0.00000003'))
+        # Can't reissue an issuance token (yet)
+        try:
+            self.nodes[0].reissueasset(issuancedata["token"], Decimal('0.00000001'))
+            raise AssertionError("You shouldn't be able to reissue a token yet")
+        except JSONRPCException:
+            pass
+
+
+        issuancedata = self.nodes[2].issueasset(Decimal('0.00000005'), 0) #5 of asset, 0 reissuance token
+        # No reissuance tokens
+        try:
+            self.nodes[2].reissueasset(issuancedata["token"], 5)
+            raise AssertionError("You shouldn't be able to reissue without a token")
+        except JSONRPCException:
+            pass
+
+        issuancedata = self.nodes[2].issueasset(0, Decimal('0.00000006')) #0 of asset, 6 reissuance token
+
+        # Node 2 will send node 1 a reissuance token, both will generate assets
+        self.nodes[2].sendtoaddress(self.nodes[1].getnewaddress(), Decimal('0.00000001'), "", "", False, issuancedata["token"])
+        # node 1 needs to know about a (re)issuance to reissue itself
+        self.nodes[1].importaddress(self.nodes[2].gettransaction(issuancedata["txid"])["details"][0]["address"])
+        # also send some bitcoin
+        self.nodes[2].generate(1)
+        self.sync_all()
+
+        assert_equal(self.nodes[2].getwalletinfo()["balance"][issuancedata["token"]], Decimal('0.00000005'))
+        assert_equal(self.nodes[1].getwalletinfo()["balance"][issuancedata["token"]], Decimal('0.00000001'))
+        redata1 = self.nodes[1].reissueasset(issuancedata["asset"], Decimal('0.05'))
+        redata2 = self.nodes[2].reissueasset(issuancedata["asset"], Decimal('0.025'))
+
+        self.sync_all()
+        # Watch-only issuances won't show up in wallet until confirmed
+        self.nodes[1].generate(1)
+        self.sync_all()
+
+        # Now have node 0 audit these issuances
+        blindingkey1 = self.nodes[1].dumpissuanceblindingkey(redata1["txid"], redata1["vin"])
+        blindingkey2 = self.nodes[2].dumpissuanceblindingkey(redata2["txid"], redata2["vin"])
+        blindingkey3 = self.nodes[2].dumpissuanceblindingkey(issuancedata["txid"], issuancedata["vin"])
+
+        # Need addr to get transactions in wallet. TODO: importissuances?
+        txdet1 = self.nodes[1].gettransaction(redata1["txid"])["details"]
+        txdet2 = self.nodes[2].gettransaction(redata2["txid"])["details"]
+        txdet3 = self.nodes[2].gettransaction(issuancedata["txid"])["details"]
+
+        # Receive addresses added last
+        addr1 = txdet1[len(txdet1)-1]["address"]
+        addr2 = txdet2[len(txdet2)-1]["address"]
+        addr3 = txdet3[len(txdet3)-1]["address"]
+
+        assert_equal(len(self.nodes[0].listissuances()), 6);
+        self.nodes[0].importaddress(addr1)
+        self.nodes[0].importaddress(addr2)
+        self.nodes[0].importaddress(addr3)
+
+        issuances = self.nodes[0].listissuances()
+        assert_equal(len(issuances), 9)
+
+        for issue in issuances:
+            if issue['txid'] == redata1["txid"] and issue['vin'] == redata1["vin"]:
+                assert_equal(issue['assetamount'], Decimal('-1'))
+            if issue['txid'] == redata2["txid"] and issue['vin'] == redata2["vin"]:
+                assert_equal(issue['assetamount'], Decimal('-1'))
+            if issue['txid'] == issuancedata["txid"] and issue['vin'] == issuancedata["vin"]:
+                assert_equal(issue['assetamount'], Decimal('-1'))
+                assert_equal(issue['tokenamount'], Decimal('-1'))
+
+        self.nodes[0].importissuanceblindingkey(redata1["txid"], redata1["vin"], blindingkey1)
+        self.nodes[0].importissuanceblindingkey(redata2["txid"], redata2["vin"], blindingkey2)
+        self.nodes[0].importissuanceblindingkey(issuancedata["txid"], issuancedata["vin"], blindingkey3)
+
+        issuances = self.nodes[0].listissuances()
+
+        for issue in issuances:
+            if issue['txid'] == redata1["txid"] and issue['vin'] == redata1["vin"]:
+                assert_equal(issue['assetamount'], Decimal('0.05'))
+            if issue['txid'] == redata2["txid"] and issue['vin'] == redata2["vin"]:
+                assert_equal(issue['assetamount'], Decimal('0.025'))
+            if issue['txid'] == issuancedata["txid"] and issue['vin'] == issuancedata["vin"]:
+                assert_equal(issue['assetamount'], Decimal('0'))
+                assert_equal(issue['tokenamount'], Decimal('0.00000006'))
+
+        # Check for value accounting when asset issuance is null but token not, ie unblinded
+        issued = self.nodes[0].issueasset(0, 1, False)
+        assert(issued["asset"] not in self.nodes[0].getwalletinfo()["balance"])
+        assert_equal(self.nodes[0].getwalletinfo()["balance"][issued["token"]], 1)
+
+
+        # Check for value when receiving defferent assets by same address.
+        self.nodes[0].sendtoaddress(unconfidential_address2, Decimal('0.00000001'), "", "", False, test_asset)
+        self.nodes[0].sendtoaddress(unconfidential_address2, Decimal('0.00000002'), "", "", False, test_asset)
+        self.nodes[0].generate(1)
+        self.sync_all()
+        received_by_address = self.nodes[1].listreceivedbyaddress(0, False, True)
+        multi_asset_amount = [x for x in received_by_address if x['address'] == unconfidential_address2][0]['amount']
+        assert_equal(multi_asset_amount['bitcoin'], value1 + value3 )
+        assert_equal(multi_asset_amount[test_asset], Decimal('0.00000003'))
+
+        # Check blinded multisig functionality
+        # Get two pubkeys
+        blinded_addr = self.nodes[0].getnewaddress()
+        pubkey = self.nodes[0].validateaddress(blinded_addr)["pubkey"]
+        blinded_addr2 = self.nodes[1].getnewaddress()
+        pubkey2 = self.nodes[1].validateaddress(blinded_addr2)["pubkey"]
+        pubkeys = [pubkey, pubkey2]
+        # Add multisig address
+        unconfidential_addr = self.nodes[0].addmultisigaddress(2, pubkeys)
+        self.nodes[1].addmultisigaddress(2, pubkeys)
+        self.nodes[0].importaddress(unconfidential_addr)
+        self.nodes[1].importaddress(unconfidential_addr)
+        # Use blinding key from node 0's original getnewaddress call
+        blinding_pubkey = self.nodes[0].validateaddress(blinded_addr)["confidential_key"]
+        blinding_key = self.nodes[0].dumpblindingkey(blinded_addr)
+        # Create blinded address from p2sh address and import corresponding privkey
+        blinded_multisig_addr = self.nodes[0].createblindedaddress(unconfidential_addr, blinding_pubkey)
+        self.nodes[0].importblindingkey(blinded_multisig_addr, blinding_key)
+        self.nodes[1].importblindingkey(blinded_multisig_addr, blinding_key)
+        # Send coins to blinded multisig address and check that they were received
+        self.nodes[2].sendtoaddress(blinded_multisig_addr, 1)
+        self.sync_all()
+        assert_equal(len(self.nodes[0].listunspent(0, 0, [unconfidential_addr])), 1)
+        assert_equal(len(self.nodes[1].listunspent(0, 0, [unconfidential_addr])), 1)
+
+        self.nodes[0].generate(1)
+        self.sync_all()
+
+        # Basic checks of rawblindrawtransaction functionality
+        blinded_addr = self.nodes[0].getnewaddress()
+        addr = self.nodes[0].validateaddress(blinded_addr)["unconfidential"]
+        txid1 = self.nodes[0].sendtoaddress(blinded_addr, 1)
+        txid2 = self.nodes[0].sendtoaddress(blinded_addr, 3)
+        unspent = self.nodes[0].listunspent(0, 0)
+        assert_equal(len(unspent), 4)
+        rawtx = self.nodes[0].createrawtransaction([{"txid":unspent[0]["txid"], "vout":unspent[0]["vout"]}, {"txid":unspent[1]["txid"], "vout":unspent[1]["vout"]}], {addr:unspent[0]["amount"]+unspent[1]["amount"]-Decimal("0.2"), "fee":Decimal("0.2")})
+        # Blinding will fail with 2 blinded inputs and 0 blinded outputs
+        # since it has no notion of a wallet to fill in a 0-value OP_RETURN output
+        try:
+            self.nodes[0].rawblindrawtransaction(rawtx, [unspent[0]["blinder"], unspent[1]["blinder"]], [unspent[0]["amount"], unspent[1]["amount"]], [unspent[0]["asset"], unspent[1]["asset"]], [unspent[0]["assetblinder"], unspent[1]["assetblinder"]])
+            raise AssertionError("Shouldn't be able to blind 2 input 0 output transaction via rawblindraw")
+        except JSONRPCException:
+            pass
+
+        # Blinded destination added, can blind, sign and send
+        rawtx = self.nodes[0].createrawtransaction([{"txid":unspent[0]["txid"], "vout":unspent[0]["vout"]}, {"txid":unspent[1]["txid"], "vout":unspent[1]["vout"]}], {blinded_addr:unspent[0]["amount"]+unspent[1]["amount"]-Decimal("0.002"), "fee":Decimal("0.002")})
+        signtx = self.nodes[0].signrawtransaction(rawtx)
+
+        try:
+            self.nodes[0].sendrawtransaction(signtx["hex"])
+            raise AssertionError("Shouldn't be able to send unblinded tx with emplaced pubkey in output without additional argument")
+        except JSONRPCException:
+            pass
+
+        blindtx = self.nodes[0].rawblindrawtransaction(rawtx, [unspent[0]["blinder"], unspent[1]["blinder"]], [unspent[0]["amount"], unspent[1]["amount"]], [unspent[0]["asset"], unspent[1]["asset"]], [unspent[0]["assetblinder"], unspent[1]["assetblinder"]])
+        signtx = self.nodes[0].signrawtransaction(blindtx)
+        txid = self.nodes[0].sendrawtransaction(signtx["hex"])
+        for output in self.nodes[0].decoderawtransaction(blindtx)["vout"]:
+            if "asset" in output and output["scriptPubKey"]["type"] != "fee":
+                raise AssertionError("An unblinded output exists")
+
+        # Test fundrawtransaction with multiple assets
+        issue = self.nodes[0].issueasset(1, 0)
+        assetaddr = self.nodes[0].getnewaddress()
+        rawtx = self.nodes[0].createrawtransaction([], {assetaddr:1, self.nodes[0].getnewaddress():2}, 0, {assetaddr:issue["asset"]})
+        funded = self.nodes[0].fundrawtransaction(rawtx)
+        blinded = self.nodes[0].blindrawtransaction(funded["hex"])
+        signed = self.nodes[0].signrawtransaction(blinded)
+        txid = self.nodes[0].sendrawtransaction(signed["hex"])
+
+        # Test fundrawtransaction with multiple inputs, creating > vout.size change
+        rawtx = self.nodes[0].createrawtransaction([{"txid":txid, "vout":0}, {"txid":txid, "vout":1}], {self.nodes[0].getnewaddress():5})
+        funded = self.nodes[0].fundrawtransaction(rawtx)
+        blinded = self.nodes[0].blindrawtransaction(funded["hex"])
+        signed = self.nodes[0].signrawtransaction(blinded)
+        txid = self.nodes[0].sendrawtransaction(signed["hex"])
 
 if __name__ == '__main__':
     CTTest ().main ()
