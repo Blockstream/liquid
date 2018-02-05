21d8bfa2079e89e357d2dd31089cd3f0b7436bee
Gregory Sanders <gsanders87@gmail.com>
2018-02-05 10:28:52 -0500

    allow any number of op return outputs in a transaction

--- src/policy/policy.cpp
+++ src/policy/policy.cpp
@@ -89,7 +89,6 @@ bool IsStandardTx(const CTransaction& tx, std::string& reason, const bool witnes
         }
     }
 
-    unsigned int nDataOut = 0;
     txnouttype whichType;
     BOOST_FOREACH(const CTxOut& txout, tx.vout) {
         if (!::IsStandard(txout.scriptPubKey, whichType, witnessEnabled) && !txout.IsFee()) {
@@ -98,7 +97,6 @@ bool IsStandardTx(const CTransaction& tx, std::string& reason, const bool witnes
         }
 
         if (whichType == TX_NULL_DATA) {
-            nDataOut++;
             if ((GetBoolArg("-validatepegout", DEFAULT_VALIDATE_PEGOUT) &&
                 txout.scriptPubKey.IsPegoutScript(Params().ParentGenesisBlockHash()) &&
                 (!txout.scriptPubKey.HasValidWhitelistPegoutProof(Params().ParentGenesisBlockHash())
@@ -113,12 +111,6 @@ bool IsStandardTx(const CTransaction& tx, std::string& reason, const bool witnes
             reason = "dust";
             return false;
         }
-    }
-
-    // only one OP_RETURN txout is permitted
-    if (nDataOut > 1) {
-        reason = "multi-op-return";
-        return false;
     }
 
     return true;
