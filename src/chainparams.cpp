66ed393b9711be5da67f360616654501d38cec05
Gregory Sanders <gsanders87@gmail.com>
2018-03-28 11:40:20 -0400

    make peginconfirmationdepth a chain param

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,6 +129,7 @@ protected:
         consensus.nMinimumChainWork = uint256S(GetArg("-con_nminimumchainwork", "0x00"));
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S(GetArg("-con_defaultassumevalid", "0x00"));
+        consensus.pegin_min_depth = GetArg("-peginconfirmationdepth", DEFAULT_PEGIN_CONFIRMATION_DEPTH);
 
         nDefaultPort = GetArg("-ndefaultport", 7042);
         nPruneAfterHeight = GetArg("-npruneafterheight", 1000);
