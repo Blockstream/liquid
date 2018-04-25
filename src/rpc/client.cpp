df05a3c04eba06f1ea7184d658f99dff335edb31
Gregory Sanders <gsanders87@gmail.com>
2018-04-25 12:24:44 -0400

    getnewblockhex: add ability to pass over transactions based on mempool age

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -140,6 +140,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "bumpfee", 1, "options" },
     { "testproposedblock", 1, "acceptnonstd" },
     { "sendtomainchain", 1, "subtractfeefromamount"},
+    { "getnewblockhex", 0, "required_age"},
     // Echo with conversion (For testing only)
     { "echojson", 0, "arg0" },
     { "echojson", 1, "arg1" },
