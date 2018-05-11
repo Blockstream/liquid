a192b609c0b1a8961e2d2e2a5b3b005088401c19
Jorge Timón <jtimon@jtimon.cc>
2018-05-11 12:48:06 +0200

    CI: Increase confidential_transactions' timeout from 30s to 60s

--- qa/rpc-tests/confidential_transactions.py
+++ qa/rpc-tests/confidential_transactions.py
@@ -14,7 +14,7 @@ class CTTest (BitcoinTestFramework):
         self.setup_clean_chain = True
 
     def setup_network(self, split=False):
-        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir)
+        self.nodes = start_nodes(self.num_nodes, self.options.tmpdir, timewait=60)
         connect_nodes_bi(self.nodes,0,1)
         connect_nodes_bi(self.nodes,1,2)
         connect_nodes_bi(self.nodes,0,2)
