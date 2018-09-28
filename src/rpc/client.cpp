a3616f16f0f7a9553abef06a354c5e58aba3783a
Gregory Sanders <gsanders87@gmail.com>
2018-09-27 20:42:37 -0400

    parse bip32_counter as number for initpegoutwallet

--- src/rpc/client.cpp
+++ src/rpc/client.cpp
@@ -141,6 +141,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "testproposedblock", 1, "acceptnonstd" },
     { "sendtomainchain", 1, "subtractfeefromamount"},
     { "getnewblockhex", 0, "required_age"},
+    { "initpegoutwallet", 1, "bip32_counter"},
     // Echo with conversion (For testing only)
     { "echojson", 0, "arg0" },
     { "echojson", 1, "arg1" },
