6b7af1fccade52322902d732348e9ae3663739a4
Luke Dashjr <luke-jr+git@utopios.org>
2019-01-24 05:11:33 +0000

    GUI: Add "L-" prefix to Bitcoin units

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("μBTC");
+    case BTC: return QString("L-BTC");
+    case mBTC: return QString("mL-BTC");
+    case uBTC: return QString::fromUtf8("μL-BTC");
     default: return QString("???");
     }
 }
