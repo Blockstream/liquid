fcac4f7fb609795ef9973fe0fa211564687fadbc
Luke Dashjr <luke-jr+git@utopios.org>
2019-02-28 12:12:10 +0000

    Fix more copyright issues
    Partially reverts 8157aa853fb7e47869ded460716cc211d781236d

--- src/init.cpp
+++ src/init.cpp
@@ -539,7 +539,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/ElementsProject/elements>";
     const std::string URL_WEBSITE = "<https://www.elementsproject.org>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2015, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2009, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
